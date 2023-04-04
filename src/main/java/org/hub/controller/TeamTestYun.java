7e41a4cdcc83dd8bfbb2165b09cb3a714a735bee
yunji <ywwi720@gmail.com>
2023-04-04 15:43:07 +0900

    yunji second commit test

--- src/main/java/org/hub/controller/TeamTestYun.java
+++ src/main/java/org/hub/controller/TeamTestYun.java
@@ -3,6 +3,6 @@ package org.hub.controller;
 public class TeamTestYun {
 
 	public static void main(String[] args) {
-		System.out.println("04-04 15:10 테스트 코드");
+		System.out.println("04-04 15:42 테스트 코드");
 	}
 }
