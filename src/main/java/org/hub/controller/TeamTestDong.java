6583ffe971e10db3725f70bd5828c9cb180d6c73
DongHyun <kyu1148@gmail.com>
2023-04-04 15:34:40 +0900

    15:34 테스트

--- src/main/java/org/hub/controller/TeamTestDong.java
+++ src/main/java/org/hub/controller/TeamTestDong.java
@@ -5,5 +5,6 @@ public static void main(String[] args) {
 	System.out.println("테스트 15:13");
 	System.out.println("테스트 15:23");
 	System.out.println("테스트 15:33");
+	System.out.println("테스트 15:34");
 }
 }
