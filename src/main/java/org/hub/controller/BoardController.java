65817bee5eac97bb0c51bfe67ed40d5ed8a8c79f
DongHyun <kyu1148@gmail.com>
2023-04-05 19:13:43 +0900

    보드컨트롤러 수정

--- src/main/java/org/hub/controller/BoardController.java
+++ src/main/java/org/hub/controller/BoardController.java
@@ -2,6 +2,7 @@ package org.hub.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.extern.log4j.Log4j;
 
