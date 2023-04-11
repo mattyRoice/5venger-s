12435742ee81d8a2ecb2491bdb788742df9173a9
yunji <ywwi720@gmail.com>
2023-04-11 17:44:42 +0900

    관리자 회원가입 시 아이디 중복 검사

--- src/main/java/org/hub/controller/AdminController.java
+++ src/main/java/org/hub/controller/AdminController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import lombok.extern.log4j.Log4j;
 
@@ -39,15 +40,24 @@ public class AdminController {
 	@PostMapping("/createAccount")
 	public String adminJoin(AdminVO admin) throws Exception { 
 		log.info("관리자 회원가입 서비스 진입");
-	  
 		// 회원가입 서비스 실행 
 		adminservice.adminJoin(admin);
-	  
 		log.info("관리자 회원가입 성공");
-	  
 		return "redirect:/admin/login"; 
-		
 	}
-	 
+	
+	// 아이디 중복 검사
+	@PostMapping("/adminIdChk")
+	@ResponseBody
+	public String memberIdChkPOST(String adminId) throws Exception{
+		log.info("adminIdChk() 진입");
+		int result = adminservice.idCheck(adminId);
+		log.info("결과값 = " + result);
+		if(result != 0) {
+			return "fail";	// 중복 아이디가 존재
+		} else {
+			return "success";	// 중복 아이디 x
+		}
+	}
 	
 }
--- src/main/java/org/hub/service/AdminService.java
+++ src/main/java/org/hub/service/AdminService.java
@@ -7,4 +7,7 @@ public interface AdminService {
 	//회원가입
 	public void adminJoin(AdminVO admin) throws Exception;
 	
+	// 아이디 중복 검사
+	public int idCheck(String adminId) throws Exception;
+	
 }
